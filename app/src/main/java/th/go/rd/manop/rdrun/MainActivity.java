34a536a186158226f0558fd9e500d25b2112a411
manop.ka <manop.ka@rd.go.th>
2016-08-26 16:48:10 +0700

    Switch Activity

--- app/src/main/java/th/go/rd/manop/rdrun/MainActivity.java
+++ app/src/main/java/th/go/rd/manop/rdrun/MainActivity.java
@@ -1,7 +1,9 @@
 package th.go.rd.manop.rdrun;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    } // Main method
+
+    // Get Event from Click button
+    public void clickSignUpMain(View view) { // view ctrl+space และจะแสดงในหน้า layout ด้วย ctrl+shift+enter complete code
+        startActivity(new Intent(MainActivity.this,SignUpActivity.class));
     }
-}
+
+} // Main class
