f35837cbaaf8a991ef0c05685af6b52cfb17700b
manop.ka <manop.ka@rd.go.th>
2016-08-31 12:37:58 +0700

    Design main UI

--- app/src/main/java/th/go/rd/manop/rdrun/MainActivity.java
+++ app/src/main/java/th/go/rd/manop/rdrun/MainActivity.java
@@ -1,21 +1,31 @@
 package th.go.rd.manop.rdrun;
 
 import android.content.Intent;
+import android.opengl.ETC1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ImageView imageView;
+    private EditText userEditText, passwordEditText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Bind widget
+        imageView = (ImageView) findViewById(R.id.imageView6);
+        userEditText = (EditText) findViewById(R.id.editText5);
+        passwordEditText = (EditText) findViewById(R.id.editText6);
 
     } // Main method
 
-    // Signin click
+    // SignIn click
     public void clickSignInMain(View view) {
+        // get logo from internet
+
 
     }
     // Get Event from Click button
